f1d35f6c1943430ea6e2c2ee669eccdb2d12cb76
zaichiyikou <913475430@qq.com>
2019-12-03 21:50:09 +0800

    2019-12-3

---
+++ src/codingInterviewByJava/Singleton.java
@@ -0,0 +1,20 @@
+package codingInterviewByJava;
+
+//单例模式
+//懒汉式
+
+public class Singleton {
+	// 这种方式是最基本的实现方式，这种实现最大的问题就是不支持多线程。
+	// 在多线程不能正常工作
+	private static Singleton instance;
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		if (instance == null) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+}
