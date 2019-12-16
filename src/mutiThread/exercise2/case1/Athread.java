c53fa0c3058a6848c043f4edbd42cdc1da847171
zaichiyikou <913475430@qq.com>
2019-12-16 13:44:06 +0800

    2019-12-16 java并发练习

---
+++ src/mutiThread/exercise2/case1/Athread.java
@@ -0,0 +1,18 @@
+package mutiThread.exercise2.case1;
+
+public class Athread extends Thread {
+	private Service service;
+
+	public Athread(Service service) {
+		super();
+		this.service = service;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		service.add("a");
+	}
+
+}
---
+++ src/mutiThread/exercise2/case2/Bthread.java
@@ -0,0 +1,18 @@
+package mutiThread.exercise2.case2;
+
+public class Bthread extends Thread {
+	private Service service;
+
+	public Bthread(Service service) {
+		super();
+		this.service = service;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		service.add("b");
+	}
+
+}
