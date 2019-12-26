d5182af19c9eb185c3b08e1f6e218a9b8ee9aad3
zaichiyikou <913475430@qq.com>
2019-12-26 21:18:58 +0800

    2019-12-16 java并发练习

---
+++ src/mutiThread/exercise3/Case4.java
@@ -0,0 +1,56 @@
+package mutiThread.exercise3;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+*	多线程核心   练习题4
+*	@author  zaichiyikoua
+*	@time  2019年12月26日
+*	@description  {	interrupt()和wait() }
+*/
+
+public class Case4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Object lock = new Object();
+		Thread threadA = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					synchronized (lock) {
+						System.out.println("begin wait");
+						lock.wait();
+						System.out.println("end wait");
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}, "线程A");
+		threadA.start();
+
+		try {
+			TimeUnit.SECONDS.sleep(4);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		threadA.interrupt();
+		// 输出如下：
+//		begin wait
+//		java.lang.InterruptedException
+//			at java.lang.Object.wait(Native Method)
+//			at java.lang.Object.wait(Unknown Source)
+//			at mutiThread.exercise3.Case4$1.run(Case4.java:25)
+//			at java.lang.Thread.run(Unknown Source)
+
+		// 线程调用wait()之后，如果对该线程对象执行interrupt(),会抛出异常
+		// 在执行同步的时候，遇到线程异常而终止的话，锁会被释放
+	}
+
+}
---
+++ src/mutiThread/exercise3/Case5.java
@@ -0,0 +1,98 @@
+package mutiThread.exercise3;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+*	多线程核心   练习题5
+*	@author  zaichiyikoua
+*	@time  2019年12月26日
+*	@description  {  过早通知和解决办法  }
+*/
+
+public class Case5 {
+	// 过早通知会导致逻辑错误
+	public static void main1(String[] args) {
+		// TODO Auto-generated method stub
+		Object lock = new Object();
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					synchronized (lock) {
+						System.out.println("begin wait");
+						lock.wait();
+						System.out.println("end wait");
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}, "线程A").start();
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				synchronized (lock) {
+					System.out.println("begin notify");
+					lock.notifyAll();
+					System.out.println("end notify");
+				}
+			}
+		}, "线程B").start();
+		// 这是正常输出情况：
+//		begin wait
+//		begin notify
+//		end notify
+//		end wait
+	}
+
+	public static void main(String[] args) {
+		Object lock = new Object();
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				synchronized (lock) {
+					System.out.println("begin notify");
+					lock.notifyAll();
+					System.out.println("end notify");
+				}
+			}
+		}, "线程B").start();
+
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					synchronized (lock) {
+						System.out.println("begin wait");
+						lock.wait();
+						System.out.println("end wait");
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}, "线程A").start();
+		// 改成这样的话，先通知wait(),那么wait()也就没有必要执行了
+		// wait()永远不会被通知
+//		begin notify
+//		end notify
+//		begin wait
+
+	}
+}
