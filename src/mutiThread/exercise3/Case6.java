31fb4f06a96896a3a51d83059e71701704a95bb4
zaichiyikou <913475430@qq.com>
2019-12-28 15:20:19 +0800

    2019-12-28 java并发练习

---
+++ src/mutiThread/exercise3/Case6.java
@@ -0,0 +1,76 @@
+package mutiThread.exercise3;
+
+/*
+*	多线程核心   练习题6
+*	@author  zaichiyikoua
+*	@time  2019年12月28日
+*	@description  {	使用wait和notify实现交叉输出  }
+*/
+
+public class Case6 {
+	// 这是实现交叉输出的最重要的标志位，确定谁先打印
+	volatile private static boolean FLAG = false;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 锁
+		Object lock = new Object();
+		// 一共十个线程，交叉打印
+		for (int i = 0; i < 5; i++) {
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					try {
+						synchronized (lock) {
+							while (FLAG == true) {
+								lock.wait();
+							}
+							System.out.println("********");
+							FLAG = true;
+							lock.notifyAll();
+						}
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}).start();
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					try {
+						synchronized (lock) {
+							while (FLAG == false) {
+								lock.wait();
+							}
+							System.out.println("########");
+							FLAG = false;
+							lock.notifyAll();
+						}
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}).start();
+		}
+
+		// 输出如下：
+//		********
+//		########
+//		********
+//		########
+//		********
+//		########
+//		********
+//		########
+//		********
+//		########
+		// 实现了交叉打印
+	}
+
+}
---
+++ src/mutiThread/exercise3/Case7.java
@@ -0,0 +1,50 @@
+package mutiThread.exercise3;
+
+import java.util.concurrent.TimeUnit;
+
+/*
+*	多线程核心   练习题7
+*	@author  zaichiyikoua
+*	@time  2019年12月28日
+*	@description  {	join()的使用之 基本用法  }
+*/
+
+public class Case7 {
+	// join()的使用情况：
+	// 很多时候，主线程创建之后启动子线程，让子线程去计算复杂的运算，很多时候主线程会先于子线程结束
+	// 所以要让主线程等待子线程执行完成之后再结束
+
+	// join()的作用是使线程执行run()中的方法，当前线程阻塞，直到join()线程执行完成
+	// 这样的话，就具有了让线程排队的作用，达到了同步的效果，但是和同步不同的是，
+	// join()内部使用的wait()，所以会释放锁
+	public static void main1(String[] args) {
+		try {
+			// TODO Auto-generated method stub
+			Thread thread = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					try {
+						// TODO Auto-generated method stub
+						int value = (int) (Math.random() * 10000);
+						System.out.println(value);
+						TimeUnit.SECONDS.sleep(2);
+					} catch (Exception e) {
+						// TODO: handle exception
+						e.printStackTrace();
+					}
+				}
+			});
+			thread.start();
+			thread.join();
+			System.out.println("main中输出");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		// 输出如下:
+//		1789
+//		main中输出
+		// thread线程中先输出，main中后输出，但是这里没有进行同步操作却达到了同步的效果
+	}
+}
