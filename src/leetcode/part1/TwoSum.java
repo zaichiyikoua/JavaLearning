7dcd54d3a90dca75076980b4d77eca86e4d7c2b1
zaichiyikou <913475430@qq.com>
2020-01-13 14:09:28 +0800

    新增leetcode两数之和暴力法

--- src/leetcode/part1/TwoSum.java
+++ src/leetcode/part1/TwoSum.java
@@ -20,6 +20,8 @@ public class TwoSum {
 
     // 思路
     // 要不能重复使用数组中的元素,所以可以用hashmap,遍历数组,如果有两个数的和等于目标值,则返回这两个数
+    // 这样使用hashmap的话速度还是很快的
+//    执行用时 :3 ms, 在所有 Java 提交中击败了97.82%的用户
     @SuppressWarnings("null")
     public int[] solution(int[] nums, int target) {
         int[] result = new int[2];
@@ -40,6 +42,33 @@ public class TwoSum {
                 break;
             }
             map.put(nums[i], i);
+        }
+        return result;
+    }
+
+    // 暴力法，思路简单直接
+    // 遍历该数组，找到两个数的和为目标值
+    // 这是最慢的方法吧，但是简单直接
+//    执行用时 :51 ms, 在所有 Java 提交中击败了10.00%的用户
+
+    @SuppressWarnings("null")
+    public int[] solution2(int[] nums, int target) {
+        int[] result = new int[2];
+        // 输入值检测
+        if (nums == null && nums.length < 2) {
+            result[0] = -1;
+            result[1] = -1;
+            return result;
+        }
+        // 暴力解决，两层遍历，寻找是否存在这两个数
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
         }
         return result;
     }
